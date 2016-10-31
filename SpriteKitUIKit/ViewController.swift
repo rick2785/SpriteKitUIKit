c8e3eaa0d6d7281358f5a83d3da6cce814d28986
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MacBook-Pro.local>
2016-10-31 09:09:45 -0500

    commit
    first commit

--- SpriteKitUIKit/ViewController.swift
+++ SpriteKitUIKit/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: SKView!
+    
+    var scene:BeeScene?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.scene = BeeScene(size: CGSize(width: self.sceneView.frame.size.width, height: self.sceneView.frame.size.height))
+        self.sceneView.presentScene(scene) 
+    }
+    
+    @IBAction func OrderBreakfast(_ sender: Any) {
+        if let scene = self.scene {
+            scene.flyBee() 
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
